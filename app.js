2ddbe3e6f34305b9ed1ee4c1f989f6293fa3a93b
Sumed Tamang <xumed001@gmail.com>
2023-02-27 22:40:51 -0600

    minor JS functionality updates

--- app.js
+++ app.js
@@ -1,17 +1,57 @@
 //
 const board = document.querySelector('.board')
 const boardSize = document.querySelectorAll('.boardSize')
+const tools = document.querySelectorAll('.tools')
+
 //
 const boardColor = '#D5D5D5'
+const defaultSize = 16
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function addsHoverEffect() {
+    this.style.backgroundColor = '#96858F'
+}
+
+function eraseMode() {
+    this.style.backgroundColor = boardColor
+}
+
+function removesHoverEffect() {
+    this.style.backgroundColor = boardColor
+}
 
 
+// function drawDeafultBoard () {
+//     board.style.gridTemplateColumns = 'repeat(16, 1fr)'
+//     board.style.gridTemplateRows = 'repeat(16, 1fr)'
 
+//     for (let i = 0; i < 256; i++) {
+//         let square = document.createElement('div')
+//         square.style.backgroundColor = boardColor
+//         square.style.border = '.1px solid #6D7993'
+
+//         // square.onmouseover = addsHoverEffect
+//         // square.onmouseleave = removesHoverEffect
+
+//         square.onmousedown = eraseMode
+        
+
+//         board.insertAdjacentElement('beforeend', square)
+//     }
+// }
 
 boardSize.forEach((element) => {
     element.addEventListener('click', () => {
         let userInput = Number(element.id)
         console.log(userInput)
         drawNewBoard(userInput)
+    })
+})
+
+tools.forEach((element) => {
+    element.addEventListener('click', () => {
+        let currentTool = element.id
+        console.log(currentTool)
     })
 })
 
@@ -29,41 +69,13 @@ function drawNewBoard (x) {
         square.style.backgroundColor = boardColor
         square.style.border = '.1px solid #6D7993'
 
-        square.onmouseover = function() {
-            this.style.backgroundColor = '#96858F'
-        }
-
-        board.insertAdjacentElement('beforeend', square)
-    }
-}
-
-function addsHoverEffect() {
-    this.style.backgroundColor = '#96858F'
-}
-
-function removesHoverEffect() {
-    this.style.backgroundColor = boardColor
-}
-
-
-function drawDeafultBoard () {
-    board.style.gridTemplateColumns = 'repeat(16, 1fr)'
-    board.style.gridTemplateRows = 'repeat(16, 1fr)'
-
-    for (let i = 0; i < 256; i++) {
-        let square = document.createElement('div')
-        square.style.backgroundColor = boardColor
-        square.style.border = '.1px solid #6D7993'
-
         square.onmouseover = addsHoverEffect
-        square.onmouseleave = removesHoverEffect
-        
 
         board.insertAdjacentElement('beforeend', square)
     }
 }
 
-drawDeafultBoard()
+drawNewBoard(defaultSize)
 
 
 
